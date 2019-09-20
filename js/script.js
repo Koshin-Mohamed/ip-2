8326541244e8bff8c9f87e633cfc8dfa9e68f6df
Koshin-Mohamed <koshin.moh@gmail.com>
2019-09-20 10:56:18 +0300

    add function and comments to js and linked to html, also added fieldsets to html.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,35 @@
+function getDD() {
+  var DD = document.getElementById("dob").value;
+  DD.slice(0, 2);
+  
+}
+// DD is for the day of the month
+
+function getMM() {
+  var MM = document.getElementById("dob").value;
+  MM.slice(3, 5);
+}
+// MM is for the month of the year
+
+function getCC() {
+  var CC = document.getElementById("dob").value;
+  CC.slice(6, 8);
+}
+// CC is for the century
+
+function getYY() {
+  var YY = document.getElementById("dob").value;
+  CC.slice(8);
+}
+// YY is for the year of the century
+
 var male = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 var female = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+//Ghanaian names in order from Sunday to Saturday
 
+function dow() {
+  var formula =
+    (CC / 4 - 2 * CC - 1 + (5 * YY) / 4 + (26 * (MM + 1)) / 10 + DD) % 7;
+  // Date of Week equation
+  alert(formula);
+}
