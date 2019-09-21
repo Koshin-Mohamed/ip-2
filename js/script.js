7da49d51f6886de5be8c86618da80658592fda08
Koshin-Mohamed <koshin.moh@gmail.com>
2019-09-21 12:28:46 +0300

    add change of label location of radio buttons and legend title, as well as new function in js.

--- js/script.js
+++ js/script.js
@@ -1,37 +1,28 @@
+// DD is for the day of the month
 function getDD() {
   var DD = document.getElementById("dob").value;
   return DD.slice(0, 2);
 }
-// DD is for the day of the month
 
+// MM is for the month of the year
 function getMM() {
   var MM = document.getElementById("dob").value;
   return MM.slice(3, 5);
 }
-// MM is for the month of the year
 
+// CC is for the century
 function getCC() {
   var CC = document.getElementById("dob").value;
   return CC.slice(6, 8);
 }
-// CC is for the century
 
+// YY is for the year of the century
 function getYY() {
   var YY = document.getElementById("dob").value;
   return YY.slice(8);
 }
-// YY is for the year of the century
 
-var gender = document.querySelector('input[name="gender"]:checked').value;
-
-// YY is for the year of the century
-
-// Date of Week equation
-
-var male = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-var female = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-//Ghanaian names in order from Sunday to Saturday
-
+// Date of Week function
 function dow() {
   var formula =
     (getCC() / 4 -
@@ -43,11 +34,19 @@ function dow() {
     7;
 
   var date = new Date(getCC() + getYY() + "/" + getMM() + "/" + getDD());
-  var day = date.getDay(); 
-
-
+  var day = date.getDay();
 }
 
+// Radio result and Akan name function & Ghanaian names in order from Sunday to Saturday
 
+function akan() {
+  var male = document.getElementById("male");
+  var female = document.getElementById("female");
 
+  var maleName = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+  var femaleName = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
+  if ((dow(0)) && male.checked == true) {
+    alert("Your Akan name is " + maleName[0] + "!")
+  }
+}
